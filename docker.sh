0305ab68d29de61549cd6fc160f3b6634ac12d89
Gregory Kman <gregorykman@gmail.com>
2018-03-05 22:00:32 -0600

    Use docker shell command to simplify container

---
+++ docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+create() {
+  docker build . -t my-ubuntu:latest
+  docker run --rm -it --name ubuntu -d my-ubuntu:latest
+}
+
+destroy() {
+  docker kill ubuntu
+}
+
+echo "1) Create and Run"
+echo "2) Stop and Destroy"
+echo "Please enter selection: "
+
+read choice
+
+case $choice in
+1)
+  create
+  ;;
+2)
+  destroy
+  ;;
+*)
+  echo "Your choice could not be parsed!"
+  ;;
+esac
